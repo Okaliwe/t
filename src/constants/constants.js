8ceb299afd935c812bde118c53239c08fe0aa1bd
Okaliwe <93863367+Okaliwe@users.noreply.github.com>
2023-04-07 03:49:00 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'Aroma of a life Time',
+    description: "Using React, Javascript,CSS,Html.Built a beautiful restaurant website for a client from start to finish. The App is called Grestaurant and it is a simple restaurant media app that allows users to see their  interesting choice menu.",
+      image: '/images/1-2.png',
+      tags: ['Javascript', 'Html', 'React', 'css'],
+    source: 'https://grestaurant.vercel.app',
+    visit: 'https://github.com/Okaliwe/grestaurant.git',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description:"While building it learnt many advanced React & JavaScript topics, as well as how to use for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    image: '/images/3-4.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://cakes-one.vercel.app',
+    visit: 'https://github.com/Okaliwe/cart.git',
+    id: 1,
+  },
+  {
+    title: 'CAKES',
+    description: "Just graduated from univelcity diploma program and built this after learning react.",
+      image: '/images/3-5.png',
+      tags: ['React', 'javascript','html','css'],
+    source: 'https://cakes-opal.vercel.app',
+    visit: 'https://github.com/Okaliwe/cakes.git',
+    id: 2,
+  },
+  {
+    title: 'HooBank',
+    description: "Used the app to learn tailwind css and next js technology.It was a rewarding experience ",
+    image: '/images/3-6.png',
+    tags: ['React', 'next.js', 'tailwind css','styled component'],
+    source: 'https://bank-ashy.vercel.app',
+    visit: 'https://github.com/Okaliwe/bank.git',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: 'started my journry at univelcity institute lagos', },
+  { year: 2021, text: 'joined Technify incubator where i began learning use of ApIsand management of states on complex applications', },
+  { year: 2022, text: 'Worked as a freelance developer ', },
+  { year: 2023, text: 'Shared my projects with the world', },
+];
